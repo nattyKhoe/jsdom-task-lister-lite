930ed8079440e18f434702ecee7f73700e583442
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-15 21:14:49 +1000

    commit tree abdbef9734cb1ec68375d31e0f2f47226b569253

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskInput = document.getElementById('new-task-description');
+  let taskList = document.getElementById('tasks');
+  let newTaskForm =document.getElementById('create-task-form');
+  let listData = {};
+
+  let priorityList = document.getElementById('priorityList');
+  // let mediumPriority = document.getElementById('medium');
+  // let lowPriority = document.getElementById('low');
+
+  newTaskForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    let delBtn = document.createElement('button');
+    let priorityList = document.getElementById('priorityList');
+    let priority = priorityList.options[priorityList.selectedIndex].text;
+    
+    let list = document.createElement('li');
+
+    list.textContent = taskInput.value;
+    if(priority==='High'){
+      list.style.backgroundColor = 'red';
+    } else if (priority==='Medium'){
+      list.style.backgroundColor = 'yellow';
+    } else if (priority==='Low'){
+      list.style.backgroundColor = 'green';
+    }
+    delBtn.innerText ='X';
+    
+    list.append(delBtn);
+    taskList.appendChild(list);
+    
+    e.target.reset();
+  });
+
+  taskList.addEventListener('click', function(e){
+    if (e.target.tagName ==='BUTTON'){
+      e.target.parentNode.remove()
+    };
+  });
 });
